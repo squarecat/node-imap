e7ef2565297486baeb0175a4d22937fed10786d7
Danielle Johnson <dinkydani@gmail.com>
2018-11-27 15:38:29 -0400

    feat(chat): crisp chat widget

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,24 @@
+const React = require('react');
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  return setPostBodyComponents([
+    <script
+      key="chat-widget"
+      dangerouslySetInnerHTML={{
+        __html: getChat(pluginOptions)
+      }}
+    />
+  ]);
+};
+
+function getChat() {
+  return `
+  window.$crisp=[];window.CRISP_WEBSITE_ID="75ceebe6-e79c-4cbf-96d2-00c8d3b8ddbf";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+  `;
+}
