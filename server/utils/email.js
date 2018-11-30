071c1c3db1f5924d1359fbf87b1876e00ddac786
Danielle Johnson <dinkydani@gmail.com>
2018-11-29 21:34:44 -0400

    fix(emails): change to mailgun for email unsub

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,33 +1,68 @@
-import nodemailer from 'nodemailer';
+import mailgun from 'mailgun-js';
+import config from 'getconfig';
 
-// create the transporter only once
-// use the default sendmail binary
-const transporter = nodemailer.createTransport({
-  sendmail: true,
-  newline: 'unix',
-  path: '/usr/sbin/sendmail'
-});
+const apiKey = config.mailgun.apiKey;
+const domain = config.mailgun.domain;
+
+const transport = mailgun({ apiKey, domain });
 
 const mailOptions = {
-  from: 'leavemealone@squarecat.io',
+  from: 'Leave Me Alone <leavemealone@squarecat.io>',
   text: 'unsubscribe'
 };
 
-export async function sendUnsubscribeMail({
-  toAddress,
-  subject = 'unsubscribe'
-}) {
+export function sendUnsubscribeMail({ toAddress, subject = 'unsubscribe' }) {
   console.log('email-utils: sending unsubscribe mail');
-  try {
-    await transporter.sendMail({
-      ...mailOptions,
-      to: toAddress,
-      subject
-    });
-    return true;
-  } catch (err) {
-    console.error('email-utils: failed to send unsubscribe mail');
-    console.error(err);
-    throw err;
-  }
+  return new Promise((resolve, reject) => {
+    transport.messages().send(
+      {
+        ...mailOptions,
+        to: toAddress,
+        subject
+      },
+      err => {
+        if (err) {
+          console.error('email-utils: failed to send unsubscribe mail');
+          console.error(err);
+          return reject(err);
+        }
+        console.log('email-utils: successfully unsubscribed');
+        return resolve(true);
+      }
+    );
+  });
 }
+
+// import nodemailer from 'nodemailer';
+
+// // create the transporter only once
+// // use the default sendmail binary
+// const transporter = nodemailer.createTransport({
+//   sendmail: true,
+//   newline: 'unix',
+//   path: '/usr/sbin/sendmail'
+// });
+
+// const mailOptions = {
+//   from: 'leavemealone@squarecat.io',
+//   text: 'unsubscribe'
+// };
+
+// export async function sendUnsubscribeMail({
+//   toAddress,
+//   subject = 'unsubscribe'
+// }) {
+//   console.log('email-utils: sending unsubscribe mail');
+//   try {
+//     await transporter.sendMail({
+//       ...mailOptions,
+//       to: toAddress,
+//       subject
+//     });
+//     return true;
+//   } catch (err) {
+//     console.error('email-utils: failed to send unsubscribe mail');
+//     console.error(err);
+//     throw err;
+//   }
+// }
--- start_dev.sh
+++ start_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./node_modules/@babel/node/bin/babel-node.js ./index.js
+NODE_ENV=development ./node_modules/@babel/node/bin/babel-node.js ./index.js
