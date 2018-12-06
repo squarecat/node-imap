98a3a3683365cf41f4a4a702138fea5c73edb376
Danielle Johnson <dinkydani@gmail.com>
2018-12-06 13:27:53 -0400

    feat(faq): faq page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -5,8 +5,38 @@ import './faq.css';
 
 const FaqPage = () => (
   <SubPageLayout className="faq-page">
-    <h1>FAQ</h1>
+    <h1>FAQs</h1>
     <div className="faqs">
+      <div className="faq-box">
+        <h3 className="faq-title">
+          How is Leave Me Alone different from Unroll.me?
+        </h3>
+        <p>
+          When we scan your inbox we NEVER sell any of your data for marketing
+          (it's been reported that Unroll.me do this, you can read about it in{' '}
+          <a href="https://www.nytimes.com/2017/04/24/technology/personal-data-firm-slice-unroll-me-backlash-uber.html">
+            The New York Times
+          </a>
+          ).
+        </p>
+        <p>
+          We unsubscribe you from the lists rather than just moving them to
+          trash or labelling them.
+        </p>
+      </div>
+      <div className="faq-box">
+        <h3 className="faq-title">How do you unsubscribe from the lists?</h3>
+        <p>If there's an unsubscribe link provided we click it.</p>
+        <p>
+          If the sender didn't provide a link then we send an email on your
+          behalf using the a unique address which identifies your subscription.
+        </p>
+        <p>
+          If the provider obeys the{' '}
+          <a href="https://tools.ietf.org/html/rfc8058">RFC</a> then this will
+          work.
+        </p>
+      </div>
       <div className="faq-box">
         <h3 className="faq-title">What mail providers do you support?</h3>
         <p>
@@ -21,68 +51,22 @@ const FaqPage = () => (
           >
             roadmap
           </a>
+          .
         </p>
       </div>
       <div className="faq-box">
-        <h3 className="faq-title">
-          How is Leave Me Alone different from Unroll.me?
-        </h3>
+        <h3 className="faq-title">Which mailboxes do you scan?</h3>
         <p>
-          1. When we scan your inbox we NEVER sell any of your data for
-          marketing (it's been reported that Unroll.me do this, you can read
-          about it in{' '}
-          <a href="https://www.nytimes.com/2017/04/24/technology/personal-data-firm-slice-unroll-me-backlash-uber.html">
-            The New York Times
-          </a>
-          )
-        </p>
-        <p>
-          2. We unsubscribe you from the lists rather than just moving them to
-          trash or labelling them
+          We scan all of your mail including the trash. The only folder we
+          exclude is the spam folder.
         </p>
       </div>
       <div className="faq-box">
-        <h3 className="faq-title">How do you unsubscribe from the lists?</h3>
+        <h3 className="faq-title">Do you have a subscription service?</h3>
         <p>
-          We show you a list of your emails which are able to be unsubscribed
-          from (they must contain the "List-Unsubscribe" header which gives us
-          the information we require to be able to remove you)
-        </p>
-      </div>
-      <div className="faq-box">
-        <h3 className="faq-title">
-          How do you handle unsubscribe confirmation steps?
-        </h3>
-        <p>
-          Emails which are able to be unsubscribed from should to provide the
-          "List-Unsubscribe" header in the email meta data.
-        </p>
-        <p>
-          <a href="https://tools.ietf.org/html/rfc8058">RFC</a>
-        </p>
-      </div>
-      <div className="faq-box">
-        <h3 className="faq-title">How do you unsubscribe from the lists?</h3>
-        <p>
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-        </p>
-      </div>
-      <div className="faq-box">
-        <h3 className="faq-title">How do you unsubscribe from the lists?</h3>
-        <p>
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-        </p>
-      </div>
-      <div className="faq-box">
-        <h3 className="faq-title">How do you unsubscribe from the lists?</h3>
-        <p>
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
-          ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          Not yet! If you'd like a monthly subscription to Leave Me Alone{' '}
+          <a href="mailto:leavemalone@squarecat.io">email</a> or{' '}
+          <a href="https://twitter.com/leavemealoneapp">tweet</a> us.
         </p>
       </div>
     </div>
