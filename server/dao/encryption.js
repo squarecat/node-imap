c0f64eae83efda81eec0665d6e6ee942b407ec87
James Ivings <james.ivings@gmail.com>
2019-01-23 18:08:10 -0500

    fix: emails without some headers cause crash

--- server/dao/encryption.js
+++ server/dao/encryption.js
@@ -1,24 +1,43 @@
-import config from 'getconfig';
 import aes256 from 'aes256';
+import config from 'getconfig';
 import crypto from 'crypto';
 
 const key = config.db.encryption.password;
 
 export function encrypt(text) {
-  if (!text) return text;
-  return aes256.encrypt(key, text);
+  try {
+    if (!text) return text;
+    return aes256.encrypt(key, text);
+  } catch (err) {
+    console.error('encryption: failed to encrypt value');
+    console.error(err);
+    return text;
+  }
 }
 
 export function decrypt(text) {
-  if (!text) return text;
-  return aes256.decrypt(key, text);
+  try {
+    if (!text) return text;
+    return aes256.decrypt(key, text);
+  } catch (err) {
+    console.error('encryption: failed to decrypt value');
+    console.error(err);
+    return text;
+  }
 }
 
 export function hash(value) {
-  return crypto
-    .createHmac('sha256', key)
-    .update(value)
-    .digest('hex');
+  try {
+    if (!value) return value;
+    return crypto
+      .createHmac('sha256', key)
+      .update(value)
+      .digest('hex');
+  } catch (err) {
+    console.error('encryption: failed to hash value');
+    console.error(err);
+    return value;
+  }
 }
 
 export function isHashEqual(hashedValue, unhashedValue) {
