64a7866387fcdc498dbfd17162c2c08bea8d18d9
Danielle Johnson <dinkydani@gmail.com>
2019-01-30 17:29:38 -0500

    fix(logger): replace winston with console logs until we can log errors with winston

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,31 +1,43 @@
-import winston from 'winston';
+// import winston from 'winston';
 
-const isDev = process.env.NODE_ENV === 'development';
+// const isDev = process.env.NODE_ENV === 'development';
 
-const logger = winston.createLogger({
-  level: isDev ? 'debug' : 'info',
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
-  transports: isDev
-    ? [
-        new winston.transports.Console({
-          format: winston.format.simple()
-        })
-      ]
-    : [
-        new winston.transports.Console({
-          format: winston.format.simple()
-        }),
-        //
-        // - Write to all logs with level `info` and below to `combined.log`
-        // - Write all logs error (and below) to `error.log`.
-        //
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
-      ],
-  exitOnError: false // do not exit on handled exceptions
-});
+const logger = {
+  info: msg => {
+    return console.log(msg);
+  },
+  warn: msg => {
+    return console.warn(msg);
+  },
+  error: msg => {
+    return console.error(msg);
+  }
+};
+
+// const logger = winston.createLogger({
+//   level: isDev ? 'debug' : 'info',
+//   format: winston.format.combine(
+//     winston.format.colorize(),
+//     winston.format.json()
+//   ),
+//   transports: isDev
+//     ? [
+//         new winston.transports.Console({
+//           format: winston.format.simple()
+//         })
+//       ]
+//     : [
+//         new winston.transports.Console({
+//           format: winston.format.simple()
+//         }),
+//         //
+//         // - Write to all logs with level `info` and below to `combined.log`
+//         // - Write all logs error (and below) to `error.log`.
+//         //
+//         new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//         new winston.transports.File({ filename: 'combined.log' })
+//       ],
+//   exitOnError: false // do not exit on handled exceptions
+// });
 
 export default logger;
