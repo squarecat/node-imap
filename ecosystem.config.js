857cce4901f9361a956eeb805cb5e8c83187022d
James Ivings <james.ivings@gmail.com>
2019-03-04 12:43:24 -0500

    build: ecosystem file for easy deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,5 +11,22 @@ module.exports = {
       },
       log_date_format: 'YYYY-MM-DD HH:mm'
     }
-  ]
+  ],
+  deploy: {
+    // "production" is the environment name
+    beta: {
+      // SSH user
+      user: 'colin',
+      // SSH host
+      host: ['beta.leavemealone.xyz'],
+      // GIT remote/branch
+      ref: 'origin/beta',
+      // GIT remote
+      repo: 'git@github.com:squarecat/leavemealone.git',
+      // path in the server
+      path: '/var/www/leavemealone',
+      // post-deploy action
+      'post-deploy': './scripts/post-deploy.sh'
+    }
+  }
 };
