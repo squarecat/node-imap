8f08c3ff9a53bf77d5817bc1f29af39a79ba7b4f
James Ivings <james.ivings@gmail.com>
2019-03-05 17:28:13 -0500

    build: added logs to post-deploy

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,5 +1,10 @@
-cp /var/www/leavemealone/shared/.env ./.env
-cp /var/www/leavemealone/shared/.env.production ./.env.production
+echo "[1] copying .env..."
+cp /var/www/leavemealone/shared/.env .
+echo "[2] copying .env.production..."
+cp /var/www/leavemealone/shared/.env.production .
+echo "[3] installing dependencies..."
 yarn --prefer-offline
-yarn run build && \
+echo "[4] building..."
+yarn run build
+echo "[5] pruning dependencies..."
 yarn --production --ignore-scripts --prefer-offline
