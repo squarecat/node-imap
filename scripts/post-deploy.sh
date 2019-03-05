cbc378ec255c3695ac38b5193970831fb4dd94bf
James Ivings <james.ivings@gmail.com>
2019-03-05 15:30:40 -0500

    fix: updated colors on login page

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,4 +1,5 @@
 cp /var/www/leavemealone/shared/.env ./.env
+cp /var/www/leavemealone/shared/.env.production ./.env.production
 yarn --prefer-offline
 yarn run build && \
 yarn --production --ignore-scripts --prefer-offline
