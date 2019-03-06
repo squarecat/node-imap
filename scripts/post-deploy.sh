62f3ac571e648404c99d01a6bf398faccefdea79
James Ivings <james.ivings@gmail.com>
2019-03-06 12:34:22 -0500

    fix: no --silent flag for build step

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -5,6 +5,6 @@ cp /var/www/leavemealone/shared/.env.production .
 echo "[3/5] installing dependencies..."
 yarn --prefer-offline --silent
 echo "[4/5] building..."
-yarn run build --silent
+yarn run build
 echo "[5/5] pruning dependencies..."
 yarn --production --ignore-scripts --prefer-offline --silent
