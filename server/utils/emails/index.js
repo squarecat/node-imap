9b452e9b3227e67ca816026d88472d43e549c247
Danielle Johnson <dinkydani@gmail.com>
2019-04-04 14:00:21 +0200

    fix(email): console logs to logger statements

--- server/utils/emails/index.js
+++ server/utils/emails/index.js
@@ -1,9 +1,11 @@
+import logger from '../logger';
+
 export function sendMail(options, transport) {
   return new Promise((resolve, reject) => {
     transport.messages().send(options, err => {
       if (err) {
-        console.log('failed to send mail');
-        console.log(err);
+        logger.error('failed to send mail');
+        logger.error(err);
         return reject(err);
       }
       return resolve(true);
