465221760a8716b216d9d5d36d18ec34b8846cec
James Ivings <james.ivings@gmail.com>
2019-06-20 16:09:28 +0700

    fix: homepage responsiveness

--- src/layouts/subpage-layout.js
+++ src/layouts/subpage-layout.js
@@ -1,14 +1,13 @@
-import './subpage-layout.module.scss';
-
 import Footer from '../components/footer';
 import Header from '../components/landing/header';
 import Layout from '../layouts/layout';
 import React from 'react';
 import cx from 'classnames';
+import styles from './subpage-layout.module.scss';
 
 export default ({ title, description, children, ...visProps }) => {
-  const classes = cx('subpage', {
-    centered: visProps.centered
+  const classes = cx(styles.subpage, {
+    [styles.centered]: visProps.centered
   });
   return (
     <Layout title={title} description={description}>
