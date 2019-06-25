5b82c9cac2cd958e6f072ea119e4bc0e1f99a28f
James Ivings <james.ivings@gmail.com>
2019-06-25 17:51:07 +0700

    fix: added message text to sentry issue

--- server/rest/webhooks/sentry.js
+++ server/rest/webhooks/sentry.js
@@ -5,15 +5,25 @@ export default app => {
   app.get('/api/webhooks/sentry', async (req, res) => {
     try {
       sendMessage('🚨');
-      res.send('🚨');
+      res.send(200);
+
+      <b>${issue.title}</b>
+      sentry.io/organizations/kanbanmail/issues/${issue.id}');
     } catch (err) {
       logger.error(err);
     }
   });
   app.post('/api/webhooks/sentry', async (req, res) => {
     logger.info(JSON.stringify(req.body, null, 2));
+    const { body } = req;
+    const issue = body.data;
     try {
-      sendMessage('🚨');
+      sendMessage(
+`🚨 New Sentry issue: <code>${issue.shortId}</code>
+<b>${issue.title}</b>
+sentry.io/organizations/squarecat-0d/issues/${issue.id}
+`
+);
       res.send(200);
     } catch (err) {
       logger.error(err);
