c0a6f69253bef8e4d2544f89ba09989016dd5032
Danielle Johnson <dinkydani@gmail.com>
2019-07-08 22:20:36 +0700

    fix: sentry using res.sendStatus

--- server/rest/webhooks/sentry.js
+++ server/rest/webhooks/sentry.js
@@ -5,7 +5,7 @@ export default app => {
   app.get('/api/webhooks/sentry', async (req, res) => {
     try {
       sendMessage('🚨');
-      res.send(200);
+      res.sendStatus(200);
     } catch (err) {
       logger.error(err);
     }
