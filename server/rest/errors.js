9d1c6c92a531079f46674fb60a5896dad6dc3d1e
Danielle <dinkydani@gmail.com>
2019-09-16 11:41:57 +0800

    chore: remove console.log

--- server/rest/errors.js
+++ server/rest/errors.js
@@ -57,7 +57,6 @@ function expressErrorHandler(err, req, res, next) {
   const json = err.toJSON ? err.toJSON() : err.stack;
 
   // if we haven't already handled this error then throw a rest error
-  console.log(json);
   if (!json.handled) {
     Sentry.captureException(err);
   }
